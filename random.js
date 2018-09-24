0f45afbc3748a4033bff77f600370eaa029e853a
cs8433 <christian.hoerauf@consorsbank.de>
2018-09-24 21:13:35 +0200

    implements own random seed result

---
+++ random.js
@@ -0,0 +1,34 @@
+// the initial seed
+let seed = (new Date()).getTime();
+
+const hashCode = (str) => {
+    let hash = 0, i, chr, len;
+    if (str.length === 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+        chr = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+
+// in order to work 'Math.seed' must NOT be undefined,
+// so in any case, you HAVE to provide a Math.seed
+const seededRandom = function () {
+    const max = 1;
+    const min = 0;
+
+    seed = (seed * 9301 + 49297) % 233280;
+    var rnd = seed / 233280;
+
+    return Math.abs(min + rnd * (max - min));
+}
+
+module.exports = {
+    random: seededRandom,
+    seed: (s) => {
+        s = s || (new Date()).getTime()
+        seed = hashCode(s.toString())
+    }
+}
---
+++ random.spec.js
@@ -0,0 +1,59 @@
+const { random, seed } = require('./random')
+const jsc = require('jsverify')
+const R = require('ramda')
+
+describe('random', () => {
+    it('should generate different values every times it gets called', () => {
+        const randomResults = [random(), random(), random()]
+        expect(randomResults[0]).not.toEqual(randomResults[1])
+        expect(randomResults[0]).not.toEqual(randomResults[2])
+        expect(randomResults[1]).not.toEqual(randomResults[2])
+    })
+    it('should return values between 0 and 1 by default', () => {
+        const randomResults = [random(), random(), random()]
+        randomResults.forEach((value) => {
+            expect(value).toBeGreaterThanOrEqual(0)
+            expect(value).toBeLessThan(1)
+        })
+    })
+    
+    it('should be seedable to repeat random values', () => {
+        seed(42)
+        const randomResultsFirst = [random(), random(), random()]
+        seed(42)
+        const randomResultsSecond = [random(), random(), random()]
+        expect(randomResultsFirst).toEqual(randomResultsSecond)
+    })
+
+    it('should be seedabel with any kind of value', () => {
+        seed('sambeneli')
+        expect(random).not.toThrow()
+        expect(isNaN(random())).toBe(false)
+        seed({a: 42})
+        expect(random).not.toThrow()
+        expect(isNaN(random())).toBe(false)
+        seed((a, b) => {return a + b})
+        expect(random).not.toThrow()
+        expect(isNaN(random())).toBe(false)
+    })
+
+    it('should take seed from Date when null passed', () => {
+        spyOn(Date.prototype, 'getTime').and.returnValue(1536953832159);
+        seed(null)
+        const first = [random(), random(), random()]
+        seed((new Date()).getTime())
+        const second = [random(), random(), random()]
+        expect(first).toEqual(second);
+
+        jest.restoreAllMocks()
+    })
+})
+describe('invariant', () => {
+    fit('should never be greater than max', () => {
+        const result = jsc.checkForall(jsc.string, (seedValue) => {
+            seed(seedValue)
+            return R.all((r) => r < 1, [random(), random(), random()])
+        })
+        expect(result).toBe(true);
+    })
+})
