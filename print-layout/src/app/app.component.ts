da267683c3cea9dcd14cbfe32d83a26ba00f79d5
cs8433 <christian.hoerauf@consorsbank.de>
2019-09-07 01:00:22 +0200

    Finalize for simple use

--- print-layout/src/app/app.component.ts
+++ print-layout/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'print-layout';
+  title = 'Fleissner Schiffre Generator';
 }
